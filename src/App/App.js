efe99377f564803698de8941e9e34725e54252cd
Marothi Mahlake <marothi.codes@gmail.com>
2021-12-09 17:19:21 +0200

    PageHeader component added.

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,45 @@
 import React from "react";
-import { CssBaseline, makeStyles } from "@material-ui/core";
+import {
+  createTheme,
+  CssBaseline,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
 import SideMenu from "../components/SideMenu";
 
 import Header from "../components/Header";
+import PageHeader from "../components/PageHeader";
+import PeopleOutlineTwoToneIcon from "@material-ui/icons/PeopleOutlineTwoTone";
+
+// Styesheet.
 import "./App.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: "#3c44b126",
+    },
+    secondary: {
+      main: "#f83245",
+      light: "#f8324526",
+    },
+    background: {
+      default: "#f4f5fd",
+    },
+  },
+  shape: {
+    borderRadius: "8px",
+  },
+  overrides: {
+    // MUI Global Styles Overrides
+    MuiAppBar: {
+      root: {
+        transform: "translateZ(0)",
+      },
+    },
+  },
+});
 
 const useStyles = makeStyles({
   appMain: {
@@ -15,13 +51,18 @@ const useStyles = makeStyles({
 function App() {
   const classes = useStyles();
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
+        <PageHeader
+          title="Page Header"
+          subtitle="Page Description"
+          icon={<PeopleOutlineTwoToneIcon fontSize="large" />}
+        />
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,30 +5,55 @@ import {
   Grid,
   IconButton,
   InputBase,
+  makeStyles,
   Toolbar,
 } from "@material-ui/core";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
 import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: "#FFFFFF",
+  },
+  searchInput: {
+    fontSize: "0.8rem",
+    opacity: "0.6",
+    padding: `0px ${theme.spacing(1)}px`,
+    transition: "ease 0.6s",
+    "&:hover": {
+      backgroundColor: "#F2F2F2",
+    },
+    "& .MuiSvgIcon-root": {
+      marginRight: theme.spacing(1),
+    },
+  },
+}));
 
 export default function Header() {
+  const classes = useStyles();
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.root}>
       <Toolbar>
-        <Grid container>
+        <Grid container alignItems="center">
           <Grid item>
-            <InputBase />
+            <InputBase
+              className={classes.searchInput}
+              placeholder="Search"
+              startAdornment={<SearchIcon fontSize="small" />}
+            />
           </Grid>
           <Grid item sm />
           <Grid item>
             <IconButton>
               <Badge badgeContent={4} color="secondary">
-                <NotificationsNoneIcon />
+                <NotificationsNoneIcon fontSize="small" />
               </Badge>
             </IconButton>
             <IconButton>
               <Badge badgeContent={2} color="primary">
-                <ChatBubbleOutlineIcon />
+                <ChatBubbleOutlineIcon fontSize="small" />
               </Badge>
             </IconButton>
             <IconButton>
